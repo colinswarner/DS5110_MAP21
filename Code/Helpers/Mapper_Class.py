52a97640b5d69b9bf8028c3ee6f51e1bfa6d26ed
Christian Schroeder <cschroeder1338@gmail.com>
2021-11-29 14:54:35 -0500

    added pipeline and functions for evaluation of models and thresholds

--- Code/Helpers/Mapper_Class.py
+++ Code/Helpers/Mapper_Class.py
@@ -3,11 +3,17 @@ import geopandas as gpd
 import pandas as pd
 import folium
 import numpy as np
+import ipywidgets as widgets
+from IPython.display import display, clear_output, Markdown
 
 class Mapper:
-    def __init__(self, shp=None, data=None):
+    def __init__(self, shp=None, data=None, maps={}, years=None, periods=None, variables=None):
         self.shp = shp
         self.data = data
+        self.maps = maps
+        self.years = years
+        self.periods = periods
+        self.variables = variables
 
     def getGDF(self):
         print(os.getcwd())
@@ -19,7 +25,7 @@ class Mapper:
     def coords(self, geom):
         return [(point[1],point[0]) for point in geom.coords]
 
-    def quickMap(SELF, gdf, col):
+    def quickMap(self, gdf, col):
         if pd.api.types.is_numeric_dtype(gdf[col]):
             vmin = gdf[col].quantile(.1)
             vmax = gdf[col].quantile(.9)
@@ -33,5 +39,46 @@ class Mapper:
         periodGDF = gdf.loc[gdf.PERIOD == period]
         obsmap = self.quickMap(periodGDF.loc[periodGDF.YEAR==year].drop('points', axis=1), 'obs rel_unrel')
         return obsmap
-
+    
+    def createMapViews(self, gdf, _variables):
+        display(Markdown(f"### Creating Map Views"))
+        self.years = gdf.YEAR.unique()
+        print(f"Possible Years: {self.years}")
+        self.periods = gdf.PERIOD.unique()
+        print(f"Possible Periods: {self.periods}")
+        self.variables = _variables
+        print(f"Possible Variables: {self.variables}")
+        print(f"Creating Views:")
+        for year in self.years:
+            self.maps[year] = {}
+            for period in self.periods:
+                self.maps[year][period] = {}
+                for variable in self.variables:
+                    try:
+                        temp_df = gdf.loc[(gdf.YEAR == year) & (gdf.PERIOD == period)][['TMC',variable,'geometry']]
+                        self.maps[year][period][variable] = mapper.quickMap(temp_df, variable)
+                        print(f"- Created: {year}, {period}, {variable}")
+                    except:
+                        print(f"- Failed: {year}, {period}, {variable}")
+                        
+    def addMapViews(self, gdf, variables):
+        for variable in variables:
+            self.variables.append(variable)
+        display(Markdown(f"### Adding Map Views"))
+        print(f"Additional Variables: {variables}")
+        print(f"Creating Views:")
+        for year in self.years:
+            for period in self.periods:
+                for variable in variables:
+                    try:
+                        temp_df = gdf.loc[(gdf.YEAR == year) & (gdf.PERIOD == period)][['TMC',variable,'geometry']]
+                        self.maps[year][period][variable] = mapper.quickMap(temp_df, variable)
+                        print(f"- Created: {year}, {period}, {variable}")
+                    except:
+                        print(f"- Failed: {year}, {period}, {variable}")
+                        
+    def viewMaps(self):
+        @widgets.interact(year=self.years, period=self.periods, variable=self.variables)
+        def changeMap(year, period, variable):
+            return mapper.maps[year][period][variable]
     
